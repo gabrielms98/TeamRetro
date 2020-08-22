252eafa0bdcb87beebeb6f8f5f477bd46737d4cf
Gabriel Martins Silva <gmbiel1@gmail.com>
2020-08-21 21:36:34 -0300

    :rocket:

---
+++ client/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import { useActions, useState } from '@u3u/vue-hooks';
+
+export default {
+
+  setup() {
+    const { reAuth } = useActions('auth', [
+      'reAuth'
+    ]);
+    
+    const states = useState('auth', [
+      'user'
+    ])
+
+    reAuth();
+  }
+  
+}
+</script>
+
+<style lang="scss">
+@import 'styles/main.scss';
+</style>
---
+++ client/src/store/modules/auth.js
@@ -0,0 +1,47 @@
+import feathers from '../../feathers';
+
+export  default {
+    namespaced: true,
+    state: {
+        loading: false,
+        error: '',
+        user: null
+    },
+    actions: {
+        async reAuth({ state }) {
+            try {
+                state.loading = true;
+                const { user } = await feathers.reAuthenticate();
+                state.user = user;
+            } catch (error) {
+                console.log(error.toString());
+            }
+            state.loading = false;
+        },
+        loginWithGithub({ state }) {
+            state.loading = true;
+            async function receiveMessage(event) {
+
+                if(event.origin !== 'http://localhost:3030') return;
+
+                const { user } = await feathers.authenticate({
+                    strategy: 'jwt',
+                    accessToken: event.data.token
+                })
+
+                state.user = user;
+                state.loading = false;
+                window.removeEventListener('message', receiveMessage);
+            }
+
+            window.addEventListener('message', receiveMessage, {
+                once: true
+            });
+            window.open('http://localhost:3030/oauth/github');
+        },
+        async logOut({ state }) {
+            await feathers.logout();
+            state.user = null;
+        }
+    }
+}
